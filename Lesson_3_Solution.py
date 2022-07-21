732448a7f1200dc37d0b6d370c0cf625eebfd5b0
debbytech22 <idades489@gmail.com>
2022-07-20 23:16:21 -0700

    My module one,lesson 3 assignment

---
+++ Lesson_3_Solution.py
@@ -0,0 +1,9 @@
+#Program to find the exponential of a number
+x = int(input ("Enter a number to perform exponential operation:" ))
+y = int(input ("Enter another number to perform exponential operation:"))
+x **= y
+if y == 0:
+    print("answer equals 1")
+else:
+    print(x)
+     
